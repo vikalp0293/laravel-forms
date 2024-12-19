d10b4d48d3d9a86b1c092462555ebea5e4d7e20c
vikalp0293 <vikalp0293@gmail.com>
2024-12-19 13:11:01 +0530

    code update for laravel forms

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,6 +18,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+
  
     <link id="skin-default" rel="stylesheet" href="{{url('css/theme.css')}}">
     {!! RecaptchaV3::initJs() !!}
